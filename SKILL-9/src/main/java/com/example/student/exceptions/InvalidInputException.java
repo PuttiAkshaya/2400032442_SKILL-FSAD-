9d2675d78e376fc4871106a3c3f02591abd2e8b2
PuttiAkshaya <2400032442@kluniversity.in>
2026-03-14 18:25:02 +0530

    delete

--- SKILL-9/src/main/java/com/example/student/exceptions/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package com.example.student.exceptions;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
--- SKILL-9/src/main/java/com/example/student/exceptions/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.student.exceptions;
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
