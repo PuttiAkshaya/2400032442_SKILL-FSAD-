e31cb7b6f3dad39016913348673e3ee5d9503aaf
PuttiAkshaya <2400032442@kluniversity.in>
2026-04-09 23:35:30 +0530

    Removed Skill12 folder

--- Skill12-FullStackCRUD-ReactSpringBoot-backend/src/main/java/com/klu/Skill12_FullStackCRUD_ReactSpringBoot/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.klu.Skill12_FullStackCRUD_ReactSpringBoot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.klu.Skill12_FullStackCRUD_ReactSpringBoot.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
